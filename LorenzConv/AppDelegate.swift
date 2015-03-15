458bfd08c47dc3e9e5a3bd1196752708c3aa54ce
Serge Ivanov <lutvuk@gmail.com>
2015-03-15 18:52:44 +0300

    Completed window layout.

--- LorenzConv/AppDelegate.swift
+++ LorenzConv/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    var convParams = ConvolutionParams()
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
---
+++ LorenzConv/ConvolutionParams.swift
@@ -0,0 +1,14 @@
+//
+//  ConvolutionParams.swift
+//  LorenzConv
+//
+//  Created by Serge on 14/03/15.
+//  Copyright (c) 2015 Serge Ivanov. All rights reserved.
+//
+
+import Foundation
+
+@objc(ConvolutionParams)
+class ConvolutionParams: NSObject {
+    var ghole: Double = 2.0
+}
